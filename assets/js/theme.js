52d32e27716327b61e8a99f36601da5a967a4061
tkbala <balkumaravel@microsoft.com>
2023-10-04 22:46:58 -0700

    Default light always

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -91,7 +91,8 @@ let initTheme = (theme) => {
   if (theme == null || theme == "null") {
     const userPref = window.matchMedia;
     if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
-      theme = "dark";
+      //theme = "dark";
+      theme = "light";
     }
   }
 
